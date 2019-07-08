5850198757ef554ddfb6d0734845ede82f7b5704
Thomas Bury <tombury182@gmail.com>
2019-07-08 13:15:17 -0400

    keep nan ews

--- models_ricker/ricker_seasonal/sim_stat.py
+++ models_ricker/ricker_seasonal/sim_stat.py
@@ -69,10 +69,10 @@ dt2 = 1 # spacing between time-series for EWS computation
 rw = 1 # rolling window (compute EWS using full time-series)
 span = 1 # span (take the whole dataset as stationary)
 lags = [1,2,3] # autocorrelation lag times
-ews = ['var','ac','sd','cv','skew','kurt','smax','aic'] # EWS to compute
+ews = ['var','ac','sd','cv','skew','kurt','smax'] # EWS to compute
 ham_length = 40 # number of data points in Hamming window
 ham_offset = 0.5 # proportion of Hamming window to offset by upon each iteration
-pspec_roll_offset = 20 # offset for rolling window when doing spectrum metrics
+pspec_roll_offset = 10 # offset for rolling window when doing spectrum metrics
 
 
 
@@ -86,12 +86,13 @@ pspec_roll_offset = 20 # offset for rolling window when doing spectrum metrics
 # Noise parameters
 amp_dem_x = 0.1 # amplitude of demographic noise
 amp_dem_y = 0.1
-amp_env_x= 0.02 # amplitude of environmental noise
-amp_env_y= 0.02
+amp_env_x= 0.1 # amplitude of environmental noise
+amp_env_y= 0.1
 
-# Fixed model parameters
-alpha_b = 1/500
-alpha_nb = 1/500
+
+# Model parameters
+alpha_b = 0.01 # density dependent effects in breeding period
+alpha_nb = 0.000672 # density dependent effects in non-breeding period
 
 
 # Function dynamic - outputs the subsequent state
@@ -121,8 +122,8 @@ def de_fun(state, params, noise):
 
 
 # Growth parameters
-rbVals = np.arange(0, 3.2, 0.2)
-rnbVals = np.arange(-1, 0.2, 0.2)
+rbVals = np.arange(0, 3.2, 0.5)
+rnbVals = np.arange(-1, 0.2, 0.5)
 
 
 
@@ -161,7 +162,7 @@ for rb in rbVals:
     for rnb in rnbVals:
         
         # Set initial condition 
-        s0 = np.array([400, 200])
+        s0 = np.array([rb/alpha_b,rb/alpha_b])
         
         # Set parameter values
         params = [rb, rnb]
@@ -229,9 +230,9 @@ for rb in rbVals:
         for var in ['Non-breeding', 'Breeding']:
             
             
-            # If time-series is extinct, do not compute EWS
-            if df_traj.loc[(rb,rnb)]['Non-breeding'].sum()==0:
-                break
+#            # If time-series is extinct, do not compute EWS
+#            if df_traj.loc[(rb,rnb)]['Non-breeding'].sum()==0:
+#                break
             ews_dic = ewstools.ews_compute(df_traj.loc[(rb,rnb)][var], 
                               roll_window = rw,
                               smooth = 'Lowess',
@@ -265,216 +266,41 @@ for rb in rbVals:
         print('EWS for (rb, rnb) = (%.2f, %.2f) complete' %(rb,rnb))
     
 
-# Concatenate EWS DataFrames. Index [Growth rate, Variable, Time]
-df_ews_full = pd.concat(appended_ews).reset_index().set_index(['rb', 'rnb','Variable','Time'])
-# Concatenate power spectrum DataFrames. Index [Realisation number, Variable, Time, Frequency]
-df_pspec = pd.concat(appended_pspec).reset_index().set_index(['rb', 'rnb','Variable','Time','Frequency'])
+# Concatenate EWS DataFrames
+df_ews_full = pd.concat(appended_ews)
+# Select ews at tmax (the rest should be Nan since using rw=1)
+df_ews = df_ews_full[df_ews_full.index==tmax-1].reset_index(drop=True).set_index(['rb', 'rnb','Variable'])
 
 
-# Refine DataFrame to just have EWS data (no time dependence)
-df_ews = df_ews_full.dropna().reset_index(level=3, drop=True).reorder_levels(['Variable', 'rb', 'rnb'])
-df_pspec = df_pspec.reset_index(level=3, drop=True).reorder_levels(['Variable', 'rb', 'rnb','Frequency'])
-
-
-
-##--------------------------
-### Grid plot of some trajectories
-##--------------------------
-#
-## Set up frame and axes
-#g = sns.FacetGrid(df_ews_full.loc[(0.5,-1)].reset_index(), 
-#                  col='rb',
-#                  hue='Variable',
-#                  palette='Set1',
-#                  col_wrap=2,
-#                  sharey=False,
-#                  aspect=1.5,
-#                  height=1.8
-#                  )
-## Set plot title size
-#plt.rc('axes', titlesize=10)
-## Plot state variable
-#g.map(plt.plot, 'Time', 'State variable', linewidth=1)
-## Plot smoothing
-#g.map(plt.plot, 'Time', 'Smoothing', color='tab:orange', linewidth=1)
-## Axes properties
-#axes = g.axes
-## Assign plot label
-#plot_traj = g
-#
-### Axes properties
-##axes = g.axes
-##for i in range(len(axes)):
-##    ax=axes[i]
-##    r=rVals[i]
-##    ax.set_ylim(bottom=0, top=60)
-#
-
-
-
-#----------------
-## Plots of EWS against r value
-#----------------
-
-## Plot of EWS metrics
-#fig1, axes = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(6,6))
-#df_ews.loc['Non-breeding pop'][['Variance']].plot(ax=axes[0],title='Early warning signals')
-#df_ews.loc['Breeding pop'][['Variance']].plot(ax=axes[0],secondary_y=True)
-#df_ews.loc['Non-breeding pop'][['Coefficient of variation']].plot(ax=axes[1])
-#df_ews.loc['Breeding pop'][['Coefficient of variation']].plot(ax=axes[1],secondary_y=True)
-#df_ews.loc['Non-breeding pop'][['Lag-1 AC']].plot(ax=axes[2])
-#df_ews.loc['Breeding pop'][['Lag-1 AC']].plot(ax=axes[2],secondary_y=True)
-#df_ews.loc['Non-breeding pop'][['Skewness']].plot(ax=axes[3])
-#df_ews.loc['Breeding pop'][['Skewness']].plot(ax=axes[3],secondary_y=True)
-#
+# Concatenate power spec DataFrames
+df_pspec_full = pd.concat(appended_pspec)
+# Select pspec at tmax (the rest should be Nan since using rw=1)
+df_pspec = df_pspec_full[df_pspec_full.index==tmax-1].reset_index().set_index(['rb', 'rnb','Variable','Frequency'])
 
 
 
 
-#
-#
-##---------------------------------
-### Power spectra visualisation
-##--------------------------------
-#
-## Limits for x-axis
-#xmin = -np.pi
-#xmax = np.pi
-#
-### Post-breeding population
-#var = 'Post-breeding pop'
-#g = sns.FacetGrid(df_pspec.loc[var].loc[rVals[0:-1:(int(len(rVals)/4))]].reset_index(level=['Growth rate','Frequency']), 
-#                  col='Growth rate',
-#                  col_wrap=3,
-#                  sharey=False,
-#                  aspect=1.5,
-#                  height=1.8
-#                  )
-## Plots
-#plt.rc('axes', titlesize=10) 
-#g.map(plt.plot, 'Frequency', 'Empirical', color='k', linewidth=1)
-#g.map(plt.plot, 'Frequency', 'Fit null', color='g', linestyle='dashed', linewidth=1)
-#g.map(plt.plot, 'Frequency', 'Fit fold', color='b', linestyle='dashed', linewidth=1)
-#g.map(plt.plot, 'Frequency', 'Fit hopf', color='r', linestyle='dashed', linewidth=1)
-#
-## Axes properties
-#axes = g.axes
-## Global axes properties
-#for i in range(len(axes)):
-#    ax=axes[i]
-#    r=rVals[i]
-##    ax.set_ylim(bottom=0, top=1.1*max(df_pspec.loc[var,d]['Empirical'].loc[xmin:xmax].dropna()))
-#    ax.set_xlim(left=xmin, right=xmax)
-#    ax.set_xticks([-3,-2,-1,0,1,2,3])
-#    ax.set_title('r = %.2f' % rVals[i])
-#    # AIC weights
-#    xpos=0.7
-#    ypos=0.9
-#    ax.text(xpos,ypos,
-#            '$w_f$ = %.1f' % df_ews.loc[var,r]['AIC fold'],
-#            fontsize=9,
-#            color='b',
-#            transform=ax.transAxes)  
-#    ax.text(xpos,ypos-0.12,
-#            '$w_h$ = %.1f' % df_ews.loc[var,r]['AIC hopf'],
-#            fontsize=9,
-#            color='r',
-#            transform=ax.transAxes)
-#    ax.text(xpos,ypos-2*0.12,
-#            '$w_n$ = %.1f' % df_ews.loc[var,r]['AIC null'],
-#            fontsize=9,
-#            color='g',
-#            transform=ax.transAxes)
-## Y labels
-#for ax in axes[::3]:
-#    ax.set_ylabel('Power')
-#    
-### Specific Y limits
-##for ax in axes[:4]:
-##    ax.set_ylim(top=0.004)
-##for ax in axes[6:9]:
-##    ax.set_ylim(top=0.25)
-## Assign to plot label
-#pspec_plot_breeding=g
-#
-#
-#
-## Limits for x-axis
-#xmin = -np.pi
-#xmax = np.pi
-#
-### Post-non-breeding population
-#var = 'Post-non-breeding pop'
-#g = sns.FacetGrid(df_pspec.loc[var].loc[rVals[0:-1:(int(len(rVals)/4))]].reset_index(level=['Growth rate','Frequency']), 
-#                  col='Growth rate',
-#                  col_wrap=3,
-#                  sharey=False,
-#                  aspect=1.5,
-#                  height=1.8
-#                  )
-## Plots
-#plt.rc('axes', titlesize=10) 
-#g.map(plt.plot, 'Frequency', 'Empirical', color='k', linewidth=1)
-#g.map(plt.plot, 'Frequency', 'Fit null', color='g', linestyle='dashed', linewidth=1)
-#g.map(plt.plot, 'Frequency', 'Fit fold', color='b', linestyle='dashed', linewidth=1)
-#g.map(plt.plot, 'Frequency', 'Fit hopf', color='r', linestyle='dashed', linewidth=1)
-#
-## Axes properties
-#axes = g.axes
-## Global axes properties
-#for i in range(len(axes)):
-#    ax=axes[i]
-#    r=rVals[i]
-##    ax.set_ylim(bottom=0, top=1.1*max(df_pspec.loc[var,d]['Empirical'].loc[xmin:xmax].dropna()))
-#    ax.set_xlim(left=xmin, right=xmax)
-#    ax.set_xticks([-3,-2,-1,0,1,2,3])
-#    ax.set_title('r = %.2f' % rVals[i])
-#    # AIC weights
-#    xpos=0.7
-#    ypos=0.9
-#    ax.text(xpos,ypos,
-#            '$w_f$ = %.1f' % df_ews.loc[var,r]['AIC fold'],
-#            fontsize=9,
-#            color='b',
-#            transform=ax.transAxes)  
-#    ax.text(xpos,ypos-0.12,
-#            '$w_h$ = %.1f' % df_ews.loc[var,r]['AIC hopf'],
-#            fontsize=9,
-#            color='r',
-#            transform=ax.transAxes)
-#    ax.text(xpos,ypos-2*0.12,
-#            '$w_n$ = %.1f' % df_ews.loc[var,r]['AIC null'],
-#            fontsize=9,
-#            color='g',
-#            transform=ax.transAxes)
-## Y labels
-#for ax in axes[::3]:
-#    ax.set_ylabel('Power')
-#    
-### Specific Y limits
-##for ax in axes[:4]:
-##    ax.set_ylim(top=0.004)
-##for ax in axes[6:9]:
-##    ax.set_ylim(top=0.25)
-## Assign to plot label
-#pspec_plot_nonbreeding=g
-#
-#
+
+
+
+
+
+
 
 #------------------------------------
 ## Export data / figures
 #-----------------------------------
 
 
-## Export EWS data
-
-# Non-breeding EWS
-df_ews_nb = df_ews.loc['Non-breeding']
-df_ews_nb.to_csv('data_export/'+dir_name+'/ews_nb.csv')
-
-# Breeding EWS
-df_ews_b = df_ews.loc['Breeding']
-df_ews_b.to_csv('data_export/'+dir_name+'/ews_b.csv')
+### Export EWS data
+#
+## Non-breeding EWS
+#df_ews_nb = df_ews.loc['Non-breeding']
+#df_ews_nb.to_csv('data_export/'+dir_name+'/ews_nb.csv')
+#
+## Breeding EWS
+#df_ews_b = df_ews.loc['Breeding']
+#df_ews_b.to_csv('data_export/'+dir_name+'/ews_b.csv')
 
 #
 #### Export power spectrum (empirical data)
